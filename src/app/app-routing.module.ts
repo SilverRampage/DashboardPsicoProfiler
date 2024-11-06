bf3d3e05a163f0418fcfe5860e25de59c0ced750
EmilianoBermudez <emiliano.bermudez@prodigylearning.com>
2024-11-06 01:20:59 -0600

    Rutas corregidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AppComponent } from '../app/app.component'
 const routes: Routes = [];
+
+
+
+const app_routes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full' }, //ruta raiz a la que siempre se va a dirigir
+  { path: 'index', component: AppComponent }, //ruta publica
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
